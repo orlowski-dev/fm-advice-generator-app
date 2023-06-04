5af86fe8b5c23e2a908ac53109a9b884f302ed25
Patryk Orłowski <orlowski.dev@gmail.com>
2023-06-04 18:34:48 +0200

    advice is now displayed on document ready

--- scripts/api.js
+++ scripts/api.js
@@ -1,11 +1,17 @@
 $(document).ready(()=> {
-    $('#roll-button').on('click', ()=> {
-        $.getJSON('https://api.adviceslip.com/advice').done(function (json) {
-            $('#slip-id').text(json.slip.id);
-            $('#advice-content').html(`&ldquo;${json.slip.advice}&rdquo;`);
-        }).fail(function (e) {
-            console.log('Data getting error!');
-            console.log(e)
-        })
-    })
+
+    //get advice on document ready
+    getAndDisplayAdvice();
+
+    $('#roll-button').on('click', getAndDisplayAdvice);
 });
+
+function getAndDisplayAdvice() {
+    $.getJSON('https://api.adviceslip.com/advice').done(function (json) {
+        $('#slip-id').text(json.slip.id);
+        $('#advice-content').html(`&ldquo;${json.slip.advice}&rdquo;`);
+    }).fail(function (e) {
+        console.log('Data getting error!');
+        console.log(e)
+    })
+}
